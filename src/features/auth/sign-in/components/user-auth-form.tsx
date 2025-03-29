cd5822d73c87fbcdaf7e431f72886ba2afc26711
ARLU <arludave23@gmail.com>
2025-03-29 17:42:05 +0800

    fix: link to ref nonexistent

--- src/features/auth/sign-in/components/user-auth-form.tsx
+++ src/features/auth/sign-in/components/user-auth-form.tsx
@@ -84,7 +84,7 @@ export function UserAuthForm({
                   <div className="flex items-center justify-between">
                     <FormLabel>Password</FormLabel>
                     <Link
-                      to="/forgot-password"
+                      to="/coming-soon"
                       className="text-sm font-medium text-muted-foreground hover:opacity-75"
                     >
                       Forgot password?
