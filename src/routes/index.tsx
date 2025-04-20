4d33872badbba7e457ce39a5b5d5429f1353c536
ARLU <arludave23@gmail.com>
2025-04-20 16:21:47 +0800

    index update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,32 @@
+import { LoadingComponent } from "@/components/general-loader";
 import { coldStart } from "@/services/auth.service";
-import { createFileRoute } from "@tanstack/react-router";
-import { useEffect } from "react";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [message, setMessage] = useState<string>(
+    "System Loading. Please wait..."
+  );
+  const { navigate } = useRouter();
   useEffect(() => {
     const fetchData = async () => {
       try {
         const result = await coldStart(); // Await the result of coldStart()
         console.log("Cold start result:", result); // Log the result
+        if (result) {
+          // Perform any necessary actions with the result
+          setMessage("Initialization complete. Redirecting...");
+
+          setTimeout(() => {
+            navigate({ to: "/sign-in" });
+          }, 2000); // Redirect after 2 seconds
+        } else {
+          console.error("Cold start failed: No result returned");
+        }
       } catch (error) {
         console.error("Error during cold start:", error);
       }
@@ -19,5 +34,10 @@ function RouteComponent() {
     fetchData(); // Call the async function immediately
     console.log("cookies:", document.cookie);
   }, []);
-  return <div>Hello "/"!</div>;
+
+  return (
+    <div>
+      <LoadingComponent fullScreen message={message} />
+    </div>
+  );
 }
