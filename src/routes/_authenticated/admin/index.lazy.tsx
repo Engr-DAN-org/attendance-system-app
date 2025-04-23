cf77937888eb19f67bbf0971f7da88609843ff78
ARLU <arludave23@gmail.com>
2025-04-23 17:49:29 +0800

    coming-soon-dashbaord

--- src/routes/_authenticated/admin/index.lazy.tsx
+++ src/routes/_authenticated/admin/index.lazy.tsx
@@ -1,6 +1,8 @@
-import Dashboard from "@/components/admin/dashboard";
+// import Dashboard from "@/components/admin/dashboard";
+import ComingSoon from "@/components/coming-soon";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/_authenticated/admin/")({
-  component: Dashboard,
+  // component: Dashboard,
+  component: ComingSoon,
 });
